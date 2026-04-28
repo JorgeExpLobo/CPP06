3b939e3c28567114b51f1701d199726073eb5759
Jorge Expósito <jolfuw@MacBook-Pro-de-Jorge.local>
2026-04-28 17:01:40 +0200

    Improved test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,11 +7,20 @@ int main() {
     data.ref = "TESTING";
     Data* original = &data;
     uintptr_t raw = Serializer::serialize(original);
+
     Data* result = Serializer::deserialize(raw);
 
+    std::cout << "Original memory address: " << original << std::endl;
+    std::cout << "Serialized/Deserialized memory address: " << result << std::endl;
+
+
+    std::cout << "Original x value: " << original->x << std::endl;
+    std::cout << "Original ref value: " << original->ref << std::endl;
+    std::cout << "Serialized/Deserialized x value: " << result->x << std::endl;
+    std::cout << "Serialized/Deserialized ref value: " << result->ref << std::endl;
     if (result == original)
-        std::cout << "Both values are equal" << std::endl;
-    else std::cout << "Values are not equal" << std::endl;
+        std::cout << "Both pointers are equal" << std::endl;
+    else std::cout << "Pointers are not equal" << std::endl;
 
     return 0;
 }
